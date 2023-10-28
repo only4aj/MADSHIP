5a343ef1c21d389feb5b9578677c0b2793524fcd
Bratadipta <05deepu08@gmail.com>
2023-10-28 12:16:52 +0530

    faq js file change with work of up and down arrow

--- static/faq.js
+++ static/faq.js
@@ -1,13 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const faqItems = document.querySelectorAll(".faq");
+document.addEventListener("DOMContentLoaded", function () {
+  const faqItems = document.querySelectorAll(".faq");
 
-    faqItems.forEach(faq => {
-        const question = faq.querySelector(".question");
-        const answer = faq.querySelector(".answer");
+  faqItems.forEach((faq) => {
+    const question = faq.querySelector(".question");
+    const answer = faq.querySelector(".answer");
 
-        question.addEventListener("click", () => {
-            faq.classList.toggle("active");
-            answer.classList.toggle("active");
-        });
+    question.addEventListener("click", () => {
+      faq.classList.toggle("active");
+      answer.classList.toggle("active");
     });
+  });
 });
+
+function change(id) {
+  if (document.getElementsByClassName("fa-chevron-up").length > 0) {
+    document.querySelector("#"+id).classList.toggle("fa-chevron-down");
+    document.querySelector("#"+id).classList.remove("fa-chevron-up");
+  } else if (document.getElementsByClassName("fa-chevron-down").length > 0) {
+    document.querySelector("#"+id).classList.remove("fa-chevron-down");
+    document.querySelector("#"+id).classList.toggle("fa-chevron-up");
+  }
+}
