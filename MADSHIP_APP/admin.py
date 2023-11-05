16e086874e8347207af2aa37e26cf0a276865d9d
only4aj <onlyforanshjuneja@gmail.com>
2023-11-05 12:29:01 +0530

    admin.py

--- MADSHIP_APP/admin.py
+++ MADSHIP_APP/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Customer,ProductItem,customerdetail
+from .models import Customer,ProductItem,customerdetail,ContactForm
 # from .models import Customer
 # Register your models here.
 
 admin.site.register(Customer)
 admin.site.register(ProductItem)
 admin.site.register(customerdetail)
+admin.site.register(ContactForm)
